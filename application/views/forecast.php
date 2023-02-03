b00eff5fde139caa77a1485085163e3e23a9c3b4
shrutivs88 <shrutivs88@gmail.com>
2023-02-04 05:18:16 +0530

    modified view file.

--- application/views/forecast.php
+++ application/views/forecast.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
   <html>
     <head>
-      <title>WeaCast | <?=$title?></title>
+      <title>Weather Cast | <?=$title?></title>
       <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 
       <!-- Latest compiled and minified CSS -->
